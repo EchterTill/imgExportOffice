649a1bffcb62ba44ec9b5b284fe6b43f7fa8fe94
Till Vogelsang <67326844+TillDV@users.noreply.github.com>
2022-09-16 18:34:47 +0200

    Added PowerPoint support

--- imgex.bat
+++ imgex.bat
@@ -1,10 +1,24 @@
 @echo off
 set /P name=Datei: 
+echo %name%
 set nameb=%name:~0,-5%
+set ending=%name:~-4%
+echo %ending%
+if %ending%==docx (
+    echo Word Document detected
+    %mediapath%=word
+    echo BP1
+) else if %ending%==pptx (
+    %mediapath%="ppt"    
+) else (
+    echo Ungültige Datei
+    pause
+    exit
+)
 copy %name% %nameb%.zip
 powershell -command "Expand-Archive %nameb%.zip ./unz"
 del %nameb%.zip
 mkdir images
-xcopy /Q .\unz\word\media\*.* "images" /s
+xcopy /Q .\unz\%mediapath%\media\*.* "images" /s
 rd /Q /S "unz"
 echo "Bilder kopiert"

0b62cbd9f25d197589cb7e3b3ee009a46149fc9c
Johannes (Jotrorox) <jotrorox@gmail.com>
2024-03-08 21:08:06 +0100

    Dokumentation hinzufügen

--- imgex.bat
+++ imgex.bat
@@ -1,9 +1,14 @@
 @echo off
+
+:: Fordere den Benutzer auf, den Dateinamen einzugeben
 set /P name=Datei: 
 echo %name%
+
+:: Überprüfe, ob die Datei eine Word oder PowerPoint Datei ist
 set nameb=%name:~0,-5%
 set ending=%name:~-4%
 echo %ending%
+
 if %ending%==docx (
     echo Word Document detected
     %mediapath%=word
@@ -15,10 +20,22 @@ if %ending%==docx (
     pause
     exit
 )
+
+:: Konvertiere die word oder pptx Datei in eine zip Datei damit wir die Bilder extrahieren können
 copy %name% %nameb%.zip
+
+:: Kopiere die Bilder in den Ordner "unz" und lösche die gerade erstellte Datei
 powershell -command "Expand-Archive %nameb%.zip ./unz"
 del %nameb%.zip
+
+:: Erstelle den Ordner "images"
 mkdir images
+
+:: Kopiert alle Bilddateien in den Ordner "images"
 xcopy /Q .\unz\%mediapath%\media\*.* "images" /s
+
+:: Lösche den Ordner "unz"
 rd /Q /S "unz"
+
+:: Eine Nachricht anzeigen, dass die Bilder kopiert wurden
 echo "Bilder kopiert"

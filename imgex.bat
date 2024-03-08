137633adb85b0980fde1b289b86f7395b94c4e13
Johannes (Jotrorox) <jotrorox@gmail.com>
2024-03-08 21:42:17 +0100

    Adressing Error if file name contains spaces #1

--- imgex.bat
+++ imgex.bat
@@ -2,19 +2,19 @@
 
 :: Fordere den Benutzer auf, den Dateinamen einzugeben
 set /P name=Datei: 
-echo %name%
+echo "%name%"
 
 :: Überprüfe, ob die Datei eine Word oder PowerPoint Datei ist
-set nameb=%name:~0,-5%
-set ending=%name:~-4%
+set nameb="%~n1"
+set ending="%~x1"
 echo %ending%
 
-if %ending%==docx (
+if /I %ending%==.docx (
     echo Word Document detected
-    %mediapath%=word
+    set mediapath=word
     echo BP1
-) else if %ending%==pptx (
-    %mediapath%="ppt"    
+) else if /I %ending%==.pptx (
+    set mediapath=ppt    
 ) else (
     echo Ungültige Datei
     pause
@@ -22,17 +22,17 @@ if %ending%==docx (
 )
 
 :: Konvertiere die word oder pptx Datei in eine zip Datei damit wir die Bilder extrahieren können
-copy %name% %nameb%.zip
+copy "%name%" "%nameb%.zip"
 
 :: Kopiere die Bilder in den Ordner "unz" und lösche die gerade erstellte Datei
-powershell -command "Expand-Archive %nameb%.zip ./unz"
-del %nameb%.zip
+powershell -command "Expand-Archive '%nameb%.zip' -DestinationPath './unz'"
+del "%nameb%.zip"
 
 :: Erstelle den Ordner "images"
 mkdir images
 
 :: Kopiert alle Bilddateien in den Ordner "images"
-xcopy /Q .\unz\%mediapath%\media\*.* "images" /s
+xcopy /Q ".\unz\%mediapath%\media\*.*" "images" /s
 
 :: Lösche den Ordner "unz"
 rd /Q /S "unz"
